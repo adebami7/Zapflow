c0069d59a7f57c795533932ed4416209b6859f36
Administration <fiyinfoluwademide@gmail.com>
2025-02-23 12:43:11 -0800

    app commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
 import "./App.css";
-import Hero from "./sections/Hero";
-import Navbar from "./sections/Navbar";
+// import Hero from "./sections/Hero";
+// import Navbar from "./sections/Navbar";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Glance from "./components/Glance";
+import About from "./components/About";
+import Services from "./components/Services";
+import Features from "./components/Features";
+import Team from "./components/Team";
+import Pricing from "./components/Pricing";
+import Footer from "./components/Footer";
+
 
 export default function App() {
   return (
     <main>
-      <Navbar />
+      {/* <Navbar />
+      <Hero /> */}
+      <Header />
       <Hero />
+      <Glance />
+      <About />
+      <Services />
+      <Features /> 
+      <Team />
+      <Pricing />
+      <Footer />
     </main>
   );
 }

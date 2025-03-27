4533fcbdf752ee3c2349b3c78e2146908265deb8
Administration <fiyinfoluwademide@gmail.com>
2025-03-27 04:26:33 -0700

    new Updates

--- src/sections/ContactUs.jsx
+++ src/sections/ContactUs.jsx
@@ -75,7 +75,7 @@ function ContactUs() {
                   </p>
                 </div>
               </div>
-              <div className="bg-white rounded-lg shadow-sm overflow-hidden mt-8">
+              {/* <div className="bg-white rounded-lg shadow-sm overflow-hidden mt-8">
                 <iframe
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1000!2d78.4867!3d17.3850!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTfCsDIzJzA2LjAiTiA3OMKwMjknMTIuMiJF!5e0!3m2!1sen!2sin!4v1635835853001!5m2!1sen!2sin"
                   width="100%"
@@ -84,7 +84,7 @@ function ContactUs() {
                   allowFullScreen=""
                   loading="lazy"
                 ></iframe>
-              </div>
+              </div> */}
             </div>
           </div>
         </div>

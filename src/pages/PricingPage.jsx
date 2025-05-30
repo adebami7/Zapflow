132835000605dcf17ff6634314160e23b7fb55cd
samuelloso <samuel@dojah.io>
2025-05-30 01:31:08 +0100

    feat: pricing update

--- src/pages/PricingPage.jsx
+++ src/pages/PricingPage.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import React, { useState } from "react";
+import { useState } from "react";
 import Button from "../components/Button";
 import { Check, X } from "lucide-react";
 
@@ -112,6 +112,18 @@ function PricingPage() {
       premium: true,
       enterprise: "Custom",
     },
+    {
+      feature: "Add-ons",
+      free: "Extra sms credits, Extra email contacts, Extra WhatsApp number for automation",
+      basic:
+        "Extra sms credits, Extra email contacts, Extra WhatsApp number for automation",
+      standard:
+        "Extra sms credits, Extra email contacts, Extra WhatsApp number for automation",
+      premium:
+        "Extra sms credits, Extra email contacts, Extra WhatsApp number for automation",
+      enterprise:
+        "Extra sms credits, Extra email contacts, Extra WhatsApp number for automation",
+    },
   ];
 
   const renderFeatureValue = (value) => {
@@ -124,6 +136,27 @@ function PricingPage() {
     }
     return <span className="text-center block">{value}</span>;
   };
+
+  const addOns = [
+    {
+      title: "Extra SMS credits",
+      sub: "Need to send more messages this month?",
+      description:
+        "Top up your SMS credits anytime to keep your campaigns running smoothly. Perfect for high-volume senders or sudden outreach needs.",
+    },
+    {
+      title: "Extra email contacts",
+      sub: "Expand your reach.",
+      description:
+        "Grow your list beyond your current contact limit by purchasing additional email contact slots. Ideal for scaling campaigns or importing large lists.",
+    },
+    {
+      title: "Extra WhatsApp number",
+      sub: "Automate more, reach wider.",
+      description:
+        "Add extra WhatsApp numbers to manage separate workflows, teams, or customer segments, all fully integrated into your automation setup.",
+    },
+  ];
 
   return (
     <>
@@ -147,71 +180,74 @@ function PricingPage() {
         </p>
         <Button className="text-base px-[65px] py-[10px]">Get Started</Button>
       </div>
-      <div className="bg-[#06348A] w-full lg:pt-[56px] lg:px-[72px] py-10 px-6 pb-[120px]">
-        <div>
-          <h2 className="text-3xl text-center sm:text-[32px] font-bold mb-4 text-[#F0F0F2] leading-[50px]">
-            We've got a plan for every{" "}
-            <span
-              className="inline-block text-[#F0F0F2] pl-2"
-              style={{
-                background:
-                  "linear-gradient(90deg, #BCF939 0%, rgba(255,255,255,0) 100%)",
-              }}
-            >
-              need
-            </span>
-          </h2>
+      <div className="bg-[#06348A]">
+        <div className="w-full max-w-[1440px] mx-auto lg:pt-[56px] lg:px-[72px] py-10 px-6 pb-[120px]">
+          <div>
+            <h2 className="text-3xl text-center sm:text-[32px] font-bold mb-4 text-[#F0F0F2] leading-[50px]">
+              We&apos;ve got a plan for every{" "}
+              <span
+                className="inline-block text-[#F0F0F2] pl-2"
+                style={{
+                  background:
+                    "linear-gradient(90deg, #BCF939 0%, rgba(255,255,255,0) 100%)",
+                }}
+              >
+                need
+              </span>
+            </h2>
 
-          <div className="mt-4 flex justify-center items-center gap-4 text-[#F0F0F2] text-base flex-wrap font-medium">
-            <span>Pay Monthly</span>
-            <label className="relative inline-flex items-center cursor-pointer">
-              <input
-                type="checkbox"
-                className="sr-only peer"
-                onChange={() => setIsYearly(!isYearly)}
-              />
-              <div className="w-12 h-6 bg-transparent border border-white rounded-full peer peer-checked:after:translate-x-full after:content-[''] after:absolute after:top-[4px] after:left-[8px] after:bg-[#BCF939] after:rounded-full after:h-4 after:w-4 after:transition-all"></div>
-            </label>
-            <span>Pay Yearly</span>
-            <button className="ml-2 bg-[#BCF939] text-[#00329B] font-medium px-4 py-1.5 rounded-full text-sm">
-              Save 30%
-            </button>
+            <div className="mt-4 flex justify-center items-center gap-4 text-[#F0F0F2] text-base flex-wrap font-medium">
+              <span>Pay Monthly</span>
+              <label className="relative inline-flex items-center cursor-pointer">
+                <input
+                  type="checkbox"
+                  className="sr-only peer"
+                  onChange={() => setIsYearly(!isYearly)}
+                />
+                <div className="w-12 h-6 bg-transparent border border-white rounded-full peer peer-checked:after:translate-x-full after:content-[''] after:absolute after:top-[4px] after:left-[8px] after:bg-[#BCF939] after:rounded-full after:h-4 after:w-4 after:transition-all"></div>
+              </label>
+              <span>Pay Yearly</span>
+              <button className="ml-2 bg-[#BCF939] text-[#00329B] font-medium px-4 py-1.5 rounded-full text-sm">
+                Save 30%
+              </button>
+            </div>
+          </div>
+
+          <div className="mt-12 grid xl:grid-cols-5 md:grid-cols-3 gap-2">
+            {pricingPlans.map((plan, index) => (
+              <div
+                key={index}
+                className="bg-[#084BC4] py-6 px-4 rounded-lg h-[285px]"
+              >
+                <p className="text-base text-[#F0F0F2] mb-4">{plan.title}</p>
+                <h1 className="text-[#F0F0F2] mb-4 font-bold text-[32px]">
+                  {plan.price
+                    ? plan.price
+                    : isYearly
+                    ? plan.yearlyPrice
+                    : plan.monthlyPrice}
+                  {plan.price !== "Custom" && (
+                    <span className="text-[#F0F0F2] text-xs font-normal">
+                      {isYearly ? "/year" : "/month"}
+                    </span>
+                  )}
+                </h1>
+                <p className="font-medium h-[32px] text-[#F0F0F2] text-xs mb-4">
+                  {plan.description}
+                </p>
+                <Link to={plan.route}>
+                  <Button variant="secondary" className="w-full py-[10px]">
+                    {plan.buttonText}
+                  </Button>
+                </Link>
+              </div>
+            ))}
           </div>
         </div>
-
-        <div className="mt-12 grid xl:grid-cols-5 md:grid-cols-3 gap-2">
-          {pricingPlans.map((plan, index) => (
-            <div
-              key={index}
-              className="bg-[#084BC4] py-6 px-4 rounded-lg h-[285px]"
-            >
-              <p className="text-base text-[#F0F0F2] mb-4">{plan.title}</p>
-              <h1 className="text-[#F0F0F2] mb-4 font-bold text-[32px]">
-                {plan.price
-                  ? plan.price
-                  : isYearly
-                  ? plan.yearlyPrice
-                  : plan.monthlyPrice}
-                {plan.price !== "Custom" && (
-                  <span className="text-[#F0F0F2] text-xs font-normal">
-                    {isYearly ? "/year" : "/month"}
-                  </span>
-                )}
-              </h1>
-              <p className="font-medium h-[32px] text-[#F0F0F2] text-xs mb-4">
-                {plan.description}
-              </p>
-              <Link to={plan.route}>
-                <Button variant="secondary" className="w-full py-[10px]">
-                  {plan.buttonText}
-                </Button>
-              </Link>
-            </div>
-          ))}
-        </div>
       </div>
+
       <div className="bg-[#F0F0F2]">
-        <div className="px-6 lg:px-[117px] py-[72px]">
+        <div className="px-6 lg:px-[117px] py-[72px] max-w-[1440px] mx-auto">
           <h2 className="text-center text-2xl mb-14 lg:text-[32px] font-bold text-[#3D414D]">
             Compare
             <span
@@ -229,22 +265,22 @@ function PricingPage() {
             <table className="w-full border border-gray-300 bg-white rounded-lg shadow-sm">
               <thead>
                 <tr>
-                  <th className="bg-gray-50 border-r border-gray-300 p-4 font-semibold text-gray-700 text-left">
+                  <th className="bg-gray-50 border-r border-gray-300 p-4 font-semibold text-[#3D414D] text-left">
                     Features
                   </th>
                   {pricingPlans.map((plan) => (
                     <th
                       key={plan.title}
-                      className={`border-r border-gray-300 p-4 text-center font-semibold text-gray-700 ${
+                      className={`border-r border-gray-300 p-4 text-center font-semibold text-[#3D414D] ${
                         plan.title === "Free"
-                          ? "bg-blue-200"
+                          ? "bg-[#CEDFFE]"
                           : plan.title === "Basic"
-                          ? "bg-green-200"
+                          ? "bg-[#E4FDB0]"
                           : plan.title === "Standard"
-                          ? "bg-yellow-200"
+                          ? "bg-[#FDEEAB]"
                           : plan.title === "Premium"
-                          ? "bg-purple-200"
-                          : "bg-cyan-200"
+                          ? "bg-[#D1D1FF]"
+                          : "bg-[#C0F5FF]"
                       }`}
                     >
                       {plan.title}
@@ -255,35 +291,83 @@ function PricingPage() {
               <tbody>
                 {comparisonData.map((row, rowIndex) => (
                   <tr key={`row-${rowIndex}`}>
-                    <td className="border-t border-r border-gray-300 p-4 font-medium text-gray-700 hover:bg-gray-50">
+                    <td className="border-t border-r border-gray-300 p-4 font-medium hover:bg-gray-50 text-[#3D414D]">
                       {row.feature}
                     </td>
-                    <td className="border-t border-r border-gray-300 p-4 text-center hover:bg-gray-50">
-                      {renderFeatureValue(row.free)}
-                    </td>
-                    <td className="border-t border-r border-gray-300 p-4 text-center hover:bg-gray-50">
-                      {renderFeatureValue(row.basic)}
-                    </td>
-                    <td className="border-t border-r border-gray-300 p-4 text-center hover:bg-gray-50">
-                      {renderFeatureValue(row.standard)}
-                    </td>
-                    <td className="border-t border-r border-gray-300 p-4 text-center hover:bg-gray-50">
-                      {renderFeatureValue(row.premium)}
-                    </td>
-                    {rowIndex === 0 ? (
+                    {rowIndex === comparisonData.length - 1 ? (
                       <td
-                        className="border-t border-gray-300 p-4 text-center hover:bg-gray-50 align-middle"
-                        rowSpan={comparisonData.length}
+                        className="border-t border-gray-300 p-4 text-center hover:bg-gray-50 text-[#3D414D]"
+                        colSpan={5}
                       >
-                        <span className="flex items-center justify-center h-full text-lg font-semibold text-gray-700">
-                          Custom
-                        </span>
+                        {renderFeatureValue(row.free)}
                       </td>
-                    ) : null}
+                    ) : (
+                      <>
+                        <td className="border-t border-r border-gray-300 p-4 text-center hover:bg-gray-50 text-[#3D414D]">
+                          {renderFeatureValue(row.free)}
+                        </td>
+                        <td className="border-t border-r border-gray-300 p-4 text-center hover:bg-gray-50 text-[#3D414D]">
+                          {renderFeatureValue(row.basic)}
+                        </td>
+                        <td className="border-t border-r border-gray-300 p-4 text-center hover:bg-gray-50 text-[#3D414D]">
+                          {renderFeatureValue(row.standard)}
+                        </td>
+                        <td className="border-t border-r border-gray-300 p-4 text-center hover:bg-gray-50 text-[#3D414D]">
+                          {renderFeatureValue(row.premium)}
+                        </td>
+                        {rowIndex === 0 ? (
+                          <td
+                            className="border-t border-gray-300 p-4 text-center hover:bg-gray-50 text-[#3D414D] align-middle"
+                            rowSpan={comparisonData.length - 1}
+                          >
+                            <span className="flex items-center justify-center h-full text-lg font-semibold text-[#3D414D]">
+                              Custom
+                            </span>
+                          </td>
+                        ) : null}
+                      </>
+                    )}
                   </tr>
                 ))}
               </tbody>
             </table>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-[#06348A]">
+        <div className="pt-[56px] max-w-[1440px] mx-auto px-6 lg:px-[72px] pb-[167px]">
+          <h2 className="text-3xl text-center sm:text-[32px] font-bold mb-4 text-[#F0F0F2] leading-[50px]">
+            Add -{" "}
+            <span
+              className="inline-block text-[#F0F0F2] pl-2"
+              style={{
+                background:
+                  "linear-gradient(90deg, #BCF939 0%, rgba(255,255,255,0) 100%)",
+              }}
+            >
+              ons
+            </span>
+          </h2>
+          <p className="max-w-[517px] mx-auto leading-7 text-center text-[#F0F0F2] mb-10">
+            Get flexible add-ons on any plan and scale as you grow. Top up to
+            get more credits without upgrading your plan.
+          </p>
+
+          <div className="mt-9 grid md:grid-cols-3 gap-6">
+            {addOns.map((addOn, index) => (
+              <div key={index} className="rounded-lg p-8 xl:h-[238px] bg-white">
+                <h1 className="text-base lg:text-2xl text-[#3D414D] mb-4">
+                  {addOn.title}
+                </h1>
+                <p className="font-medium leading-[24px] text-[#3D414D] text-xs max-w-[345px]">
+                  {addOn.sub}
+                </p>
+                <p className="font-medium leading-[24px] text-[#3D414D]  text-xs max-w-[345px]">
+                  {addOn.description}
+                </p>
+              </div>
+            ))}
           </div>
         </div>
       </div>
